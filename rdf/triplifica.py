5da6aeec682fc2b4f5a6f431e543cc6558b17fbb
Renato Fabbri <renato.fabbri@gmail.com>
2015-10-11 13:02:09 -0300

    adiantada triplificacao

--- rdf/triplifica.py
+++ rdf/triplifica.py
@@ -79,13 +79,15 @@ ns=namespaces=namespaces(["rdf","rdfs","owl","xsd", # basic namespaces
 #g.add((ouri,dct.description,r.Literal(u"Ontologia do Participa.br, levantada com base nos dados e para conectar com outras instâncias")))
 G(      ns["omar"].data0+".rdf",
         ns["dcterms"].description,
-        L("OMAR is Ontology of the Art Museum of Rio de Janeiro",lang="en")
+        L("OMAR is Ontology of the Art Museum of Rio",lang="en")
     )
 
 G(      ns["omar"].data0+".rdf",
         ns["dcterms"].description,
-        L("OMAR é a Ontologia do Museu de Arte Rio",lang="pt")
+        L("OMAR é a Ontologia do Museu de Arte do Rio",lang="pt")
     )
+
+
 isbn=[]
 keys=[]
 ta=[]
@@ -99,6 +101,114 @@ for d in data:
     keys__=list(d.keys())
     for key in keys__:
         tdict[key]+=[d[key]]
+for d in data:
+    # forma a id com uri omar + id do pergamon OK
+    wid=ID_GEN(ns["omar"].Artwork,d["pergamus_id"])
+    # adiciona as outras ids, criando uma classe para cada uma delas
+    if "Título Principal" in d.keys():
+        G(  wid,
+          ns["omar"].title,
+          L(d["Título Principal"])
+        )
+    if "Assuntos" in d.keys():
+        assuntos=d["Assuntos"].split(", ")
+        for assunto in assuntos:
+            G(  wid,
+              ns["omar"].subject,
+              L(assunto)
+            )
+    if "ISBN" in d.keys():
+        isbn_=d["ISBN"]
+        isbncount=isbn_.count("ISBN : ")
+        if " / " in isbn_:
+            isbn__,isbn1=isbn_.split(" / ")
+            isbn2=isbn1.split("ISBN : ")[-1]
+            idsbn=ID_GEN(ns["omar"].ID,isbn1)
+            G(  wid,
+              ns["omar"].id,
+              idsbn
+            )
+            G(  idsbn,
+              ns["omar"].value,
+              L(isbn1)
+            )
+            idsbn=ID_GEN(ns["omar"].ID,isbn2)
+            G(  wid,
+              ns["omar"].id,
+              idsbn
+            )
+            G(  idsbn,
+              ns["omar"].value,
+              L(isbn2)
+            )
+        elif isbncount==1:
+            isbn1=isbn_.split(" : ")[-1]
+            idsbn=ID_GEN(ns["omar"].ID,isbn1)
+            G(  wid,
+              ns["omar"].id,
+              idsbn
+            )
+            G(  idsbn,
+              ns["omar"].value,
+              L(isbn1)
+            )
+        elif isbncount==2:
+            isbn1,isbn2=isbn_.split("ISBN : ")[1:]
+            idsbn=ID_GEN(ns["omar"].ID,isbn1)
+            G(  wid,
+              ns["omar"].id,
+              idsbn
+            )
+            G(  idsbn,
+              ns["omar"].value,
+              L(isbn1)
+            )
+            idsbn=ID_GEN(ns["omar"].ID,isbn2)
+            G(  wid,
+              ns["omar"].id,
+              idsbn
+            )
+            G(  idsbn,
+              ns["omar"].value,
+              L(isbn2)
+            )
+        elif isbncount==3:
+            isbn1,isbn2,isbn3=isbn_.split("ISBN : ")[1:]
+            idsbn=ID_GEN(ns["omar"].ID,isbn1)
+            G(  wid,
+              ns["omar"].id,
+              idsbn
+            )
+            G(  idsbn,
+              ns["omar"].value,
+              L(isbn1)
+            )
+            idsbn=ID_GEN(ns["omar"].ID,isbn2)
+            G(  wid,
+              ns["omar"].id,
+              idsbn
+            )
+            G(  idsbn,
+              ns["omar"].value,
+              L(isbn2)
+            )
+            idsbn=ID_GEN(ns["omar"].ID,isbn3)
+            G(  wid,
+              ns["omar"].id,
+              idsbn
+            )
+            G(  idsbn,
+              ns["omar"].value,
+              L(isbn3)
+            )
+
+
+    # todas subclasse da classe ID
+
+    # adiciona assuntos, títulos, edição
+
+    # adiciona autor via classe entidade
+
 #    if "ISBN" in d.keys():
 #        isbn.append(d["ISisbnisbnBN"]+"\n")
 #    if "Título do Artigo" in d.keys():
@@ -112,6 +222,8 @@ for key in keys_:
 #f.writelines(ta)
 #f.close()
 
+# Fazer dicionário para as classes e propriedades relacionadas aos campos
+# fazer só "obra" e linkar com os dados via propriedades?
 
 # análise mínima dos dados
 print(len(keys_), "campos", keys_)
